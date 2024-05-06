522e4b238561a29fa127ad85122f8ebff4b94399
Pavel <wol1414@gmail.com>
2024-05-06 20:15:29 +0200

    Feat: RSS-ECOMM-1_12: Initialize Husky (#4)
    * feat: add Husky
    
    * feat: test husky
    
    * feat: add husky
    
    * doc: edit test commnand
    
    * doc: edit vitest command

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
+/* eslint-disable vue/no-reserved-component-names */
+/* eslint-disable vue/multi-word-component-names */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import PrimeVue from 'primevue/config'
+// import PrimeVue from 'primevue/config'
 
 import InputText from 'primevue/inputtext'
 import Button from 'primevue/button'
-import Tooltip from 'primevue/tooltip'
 import Icon from '@/components/common/Icon.vue'
 
 const app = createApp(App)
 
 app.component('InputText', InputText)
 app.component('Button', Button)
-app.component('Tooltip', Tooltip)
 app.component('Icon', Icon)
 
 app.use(router)
