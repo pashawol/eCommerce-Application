8f6e639ed1f3dea27a8fb42550ff287f21b12b21
Janis <janis.paberzs18@gmail.com>
2024-05-06 19:29:52 +0300

    feat: add environments file & fetch functionality (#3)

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  VITE_CTP_PROJECT_KEY: string
+  VITE_CTP_CLIENT_SECRET: string
+  VITE_CTP_CLIENT_ID: string
+  VITE_CTP_AUTH_URL: string
+  VITE_CTP_API_URL: string
+  VITE_CTP_SCOPES: string
+  VITE_ACCESS_TOKEN: string
+}
