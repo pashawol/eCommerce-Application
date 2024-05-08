29d6baf787bfe148ea88d4ea3b3ac95942dc11d0
JulietBrn <120723264+JulietBrn@users.noreply.github.com>
2024-05-08 20:55:39 +0200

    Feat/rss ecomm 2 01 implement validation for login form (#8)
    * feat: add inputs, title, buttons to login page
    
    * feat: implement validation rules for login page
    
    * refactor: move vars from loginView to formPage
    
    * fix: fix form layout

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,93 @@
 <template>
-  <div class="login">
-    <h1>This is a login page</h1>
-  </div>
+  <FormPage v-bind="params">
+    <form action="">
+      <div class="flex flex-column gap-2 mb-4">
+        <label for="username">Email</label>
+        <InputText
+          id="email"
+          v-model="email"
+          aria-describedby="email-help"
+          @input="validateEmail"
+        />
+        <small class="p-error" id="email-help">{{ emailErrorMessage }}</small>
+      </div>
+
+      <div class="flex flex-column gap-2 mb-4">
+        <label for="password">Password</label>
+        <Password
+          id="password"
+          v-model="password"
+          aria-describedby="password-help"
+          :feedback="false"
+          toggleMask
+          @input="validatePassword"
+        />
+        <small class="p-error" id="password-help">{{ passwordErrorMessage }}</small>
+      </div>
+    </form>
+  </FormPage>
 </template>
 
-<style></style>
+<script setup lang="ts">
+  import { ref } from 'vue'
+  import FormPage from '@/components/layouts/FormPage/FormPage.vue'
+  import InputText from 'primevue/inputtext'
+  import Password from 'primevue/password'
+
+  const params = {
+    title: 'Login',
+    btnName: 'Submit',
+    linkName: 'Registration',
+    linkUrl: '/registration',
+    linkText: 'Don’t have an account?'
+  }
+
+  const email = ref('')
+  const password = ref('')
+  const emailErrorMessage = ref('')
+  const passwordErrorMessage = ref('')
+
+  const validateEmail = () => {
+    const trimmedEmail: string = email.value.trim()
+
+    const validationRules = {
+      format: {
+        rule: !/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(trimmedEmail)
+      },
+      spaces: {
+        rule: trimmedEmail !== email.value
+      },
+      domain: {
+        rule: !/\.[a-zA-Z]{2,}$/.test(trimmedEmail.split('@')[1])
+      },
+      atSymbol: {
+        rule: !/@/.test(trimmedEmail)
+      }
+    }
+  }
+
+  const validatePassword = () => {
+    const trimmedPassword: string = password.value.trim()
+
+    const validationRules = {
+      spaces: {
+        rule: trimmedPassword !== password.value
+      },
+      special: {
+        rule: !/[\W_]/.test(trimmedPassword)
+      },
+      digit: {
+        rule: !/\d/.test(trimmedPassword)
+      },
+      uppercase: {
+        rule: !/[A-Z]/.test(trimmedPassword)
+      },
+      lowercase: {
+        rule: !/[a-z]/.test(trimmedPassword)
+      },
+      length: {
+        rule: trimmedPassword.length < 8
+      }
+    }
+  }
+</script>
