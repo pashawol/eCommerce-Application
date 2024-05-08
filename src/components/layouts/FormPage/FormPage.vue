fdb25cdbb8b0d52f5d4de15bbe371bff6dc133d6
JulietBrn <shishkina.ja@gmail.com>
2024-05-08 15:03:43 +0200

    feat: add inputs, title, buttons to login page

---
+++ src/components/layouts/FormPage/FormPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="FormPage">
+    <div class="FormPage__main">
+      <div class="FormPage__body">
+        <div class="text-center">
+          <h1 class="FormPage__title">{{ title }}</h1>
+        </div>
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps({
+    title: {
+      type: String,
+      default: ''
+    }
+  })
+</script>
+
+<style lang="scss">
+  @import '@/assets/scss/_common.scss';
+  @import './FormPage.scss';
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,43 @@
 <template>
-  <div class="login">
-    <h1>This is a login page</h1>
-  </div>
+  <FormPage v-bind="params">
+    <form action="">
+      <div class="flex flex-column gap-2 mb-4">
+        <label for="username">Email</label>
+        <InputText id="email" v-model="value" aria-describedby="email-help" />
+      </div>
+
+      <div class="flex flex-column gap-2 mb-4">
+        <div class="flex">
+          <label for="password">Password</label>
+        </div>
+        <Password id="password" v-model="password" aria-describedby="password-help" toggleMask />
+      </div>
+
+      <div class="mb-4">
+        <Button type="submit" :label="params.btnName" />
+      </div>
+      <div class="mb-3 text-center">
+        Don’t have an account?
+        <router-link to="/registration" class="text-primary">Registration</router-link>
+      </div>
+    </form>
+  </FormPage>
 </template>
 
-<style></style>
+<script setup>
+  import { ref } from 'vue'
+  import FormPage from '@/components/layouts/FormPage/FormPage.vue'
+  import InputText from 'primevue/inputtext'
+  import Password from 'primevue/password'
+  const params = {
+    title: 'Login',
+    btnName: 'Submit'
+  }
+
+  // const value = ref('')
+  const password = ref()
+</script>
+
+<style lang="scss">
+  /* Your component's styles go here */
+</style>
