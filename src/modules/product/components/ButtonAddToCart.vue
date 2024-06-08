69525f7c3e6e940ba76ca868c7e5645f13255e62
JulietBrn <120723264+JulietBrn@users.noreply.github.com>
2024-06-08 12:08:17 +0200

    feat: add button add to cart (#47)

---
+++ src/modules/product/components/ButtonAddToCart.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="">
+    <Button label="" class="w-full p-button-success">
+      <i class="pi pi-cart-plus" style="font-size: 1.2rem"></i>
+    </Button>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
