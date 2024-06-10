0578ec122dea42714a356b8dcfbe432ce921466a
JulietBrn <shishkina.ja@gmail.com>
2024-06-10 08:45:26 +0200

    refactor: move teamMembers data

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,56 +2,7 @@
   import Avatar from 'primevue/avatar'
   import TabView from 'primevue/tabview'
   import TabPanel from 'primevue/tabpanel'
-
-  const teamMembers = [
-    {
-      name: 'Janis Paberzs',
-      role: 'Junior Frontend Developer 🧑‍💻',
-      contribution: [
-        'Janis was responsible for setting up the commerce tools and managing user authentication, ensuring that the backend was robust and secure.',
-        "He took charge of the user profile management, making sure that user data was handled efficiently and securely, which was crucial for the application's credibility and user trust."
-      ],
-      bio: [
-        'I am a 22-year-old junior frontend developer with a passion for creating visually appealing and user-friendly web interfaces.',
-        'I am skilled in HTML, CSS, and JavaScript, and has a keen eye for detail. My enthusiasm for learning new technologies and frameworks drives me to continually improve my skill set. I enjoys collaborating with design teams to bring creative ideas to life, ensuring a seamless user experience.',
-        'Outside of coding, I am an avid problem solver and enjoys tackling new challenges.'
-      ],
-      githubUrl: 'https://github.com/jpaberzs',
-      photoUrl: 'member-1'
-    },
-    {
-      name: 'Iuliia Barinova',
-      role: 'Junior Frontend Developer 🎨',
-      contribution: [
-        'Iuliia took the lead in organizing the workflow within Jira, ensuring that all tasks were well-coordinated and deadlines were met.',
-        'She played a crucial role in adding products to the commerce tools and displaying the product catalog in the application, making sure that the application was both functional and user-friendly.',
-        "As the inspiration behind the idea of the clothing store, Iuliia's vision and passion for fashion drove the project's direction and success."
-      ],
-      bio: [
-        "I'm a Junior Frontend Developer with a deep enthusiasm for web development. My journey into programming started about a year ago when I made a decision to leave my previous job as an accountant and fully immerse myself in the world of web design and development.",
-        "I studied at St Petersburg University where I earned my master's degree in economics and when I got a chance to try myself in programming I truly found my calling."
-      ],
-      githubUrl: 'https://github.com/JulietBrn',
-      photoUrl: 'member-2',
-      additionalClass: 'team-member_right'
-    },
-    {
-      name: 'Pavel Khalunin',
-      role: 'Frontend Developer, freelancer 💻',
-      contribution: [
-        'Pavel has been instrumental in setting up the project and organizing the workflow with Vue. His expertise ensured the smooth integration of various project components.',
-        "One of Pavel's key contributions was the implementation of the product card display within the application, which significantly enhanced the user experience."
-      ],
-      bio: [
-        'I was born and grew up in Belarus, in the city of Svetlogorsk. Since childhood I was fascinated by computers and the internet, and dreamed of creating my own websites. I started learning HTML and CSS in 2012 on my own, with the help of online courses, books and articles. I did different projects for myself and for my friends, and gradually gained experience and confidence in my skills.',
-        'In 2017, I got a job at a small web studio where I did website layout for different clients. I worked in a team with other developers, designers and managers and I loved what I was doing. I learnt new technologies such as Bootstrap, Sass, jQuery and others and tried to do my work qualitatively and on time.',
-        'After I continued working as a layout designer as a freelancer, where I got more interesting and challenging assignments',
-        'In 2023 I moved to Poland, to the city of Warsaw. I also started learning JavaScript, because I realised that it is an important and in-demand programming language that allows you to create dynamic and interactive websites. I wanted to expand my capabilities and become a full-fledged web developer, not just a layout designer.'
-      ],
-      githubUrl: 'https://github.com/pashawol',
-      photoUrl: 'member-3'
-    }
-  ]
+  import { teamMembers } from '@/components/utils/teamMembers'
 </script>
 
 <template>
